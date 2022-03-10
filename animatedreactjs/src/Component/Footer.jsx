2d336d06d8f078654302e1222e201eaf52847c6e
Malkesh Dalia <malkesh2911@hotmail.com>
2022-03-10 16:59:52 +0530

    updated

--- animatedreactjs/src/Component/Footer.jsx
+++ animatedreactjs/src/Component/Footer.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer className="w-100 bg-light text-center"></footer>
-      <p>All Rights Reserved</p>
+      <footer className="w-100 bg-light text-center">
+        <p>All Rights Reserved</p>
+      </footer>
     </>
   );
 };
